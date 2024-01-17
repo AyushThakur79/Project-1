236745d133d7eea3de1d85dcb871b6b2f27f8b6a
Ayush Thakur <150569577+AyushThakur79@users.noreply.github.com>
2024-01-17 18:50:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // lenis for smmoth scrolling 
 
-// const lenis = new Lenis()
+const lenis = new Lenis()
 
-// lenis.on('scroll', (e) => {
-//   console.log(e)
-// })
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
 
-// function raf(time) {
-//   lenis.raf(time)
-//   requestAnimationFrame(raf)
-// }
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
 
-// requestAnimationFrame(raf)
+requestAnimationFrame(raf)
 
 // ends
 
@@ -49,3 +49,24 @@ function swipperAnimation() {
 }
 
 swipperAnimation();
+
+var menu = document.querySelector(".menu h5");
+var menu2 = document.querySelector(".menu");
+var menuscr = document.querySelector(".menu-scr");
+var navimg = document.querySelector("nav img");
+var flag = 0;
+
+menu.addEventListener("click", function () {
+	if (flag == 0) {
+		menuscr.style.top = 0;
+		navimg.style.opacity = 0;
+		menu2.style.position = "fixed"
+		menu2.style.padding = "0vw 66vw"
+		flag = 1;
+	} else {
+		menuscr.style.top = "-100%";
+		navimg.style.opacity = 1;
+		menu2.style.padding = "0vw 66vw"
+		flag = 0;
+	}
+});
